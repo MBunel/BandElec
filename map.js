76682be242cef1f2bc7dc365ea297cbc6e800afb
Mattia <mattiabunel@hotmail.fr>
2017-05-10 00:15:21 +0200

    Commit initial

---
+++ map.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const defaultVue = {
+    coords: [43.7, 7.25],
+    zoom: 11
+};
+
+// Map creation
+var map = L.map('mapid')
+    .setView(defaultVue.coords, defaultVue.zoom);
+
+// TileLayer definition
+var TileLayer = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+}).addTo(map);
