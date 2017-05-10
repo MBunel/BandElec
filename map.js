8e090023db2cf0d2ee7d9995399cd46cd8ea150c
Mattia <mattiabunel@hotmail.fr>
2017-05-10 15:41:44 +0200

    Ajout couleur pattern

--- map.js
+++ map.js
@@ -18,4 +18,29 @@ var TileLayer = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite
     ext: 'png'
 }).addTo(map);
 
-omnivore.kml('dep.kml').addTo(map);
+var pattern = new L.Pattern({width:50});
+
+var shape = new L.PatternRect({width:25, color:'red',stroke:false, fill: true}),
+    shape2 = new L.PatternRect({x:25, width:15, color:'blue',stroke:false, fill: true}),
+    shape3 = new L.PatternRect({x:40, width:10,  color:'green',stroke:false, fill: true});
+
+pattern.addShape(shape);
+pattern.addShape(shape2);
+pattern.addShape(shape3);
+
+pattern.addTo(map);
+
+
+function style(feature) {
+    return {
+	fillPattern: pattern,
+	fillOpacity: 0.8,
+	stroke: false
+    };
+}
+
+var custonLayer = L.geoJson(null, {style: style});
+
+var Dept = omnivore.kml('dep.kml', null, custonLayer);
+
+custonLayer.addTo(map);
